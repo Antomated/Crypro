28b6aba536b12dc75a03c04d618628287168941a
beavean <beavean@gmail.com>
2024-04-01 22:42:05 +0200

    Initial Commit

---
+++ Crypro/CryproApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryproApp.swift
+//  Crypro
+//
+//  Created by Anton Petrov on 01.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
