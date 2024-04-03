d87bbbbcc1eae91dcbcf9921a9da279e047b8e56
beavean <beavean@gmail.com>
2024-04-04 00:35:11 +0200

    Add search bar view with end editing methods

--- Crypro/Core/Components/SearchBar/SearchBarView.swift
+++ Crypro/Core/Components/SearchBar/SearchBarView.swift
@@ -5,4 +5,47 @@
 //  Created by Anton Petrov on 03.04.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct SearchBarView: View {
+    @Binding var searchText: String
+
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundStyle(
+                    searchText.isEmpty ? Color.theme.secondaryText : Color.theme.accent
+                )
+
+            TextField("Search by name or symbol...", text: $searchText)
+                .foregroundStyle(Color.theme.accent)
+                .autocorrectionDisabled()
+                .keyboardType(.alphabet)
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10)
+                        .foregroundStyle(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            UIApplication.shared.endEditing()
+                            searchText = ""
+                        }
+                    , alignment: .trailing
+                )
+        }
+        .font(.callout)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .strokeBorder(Color.theme.secondaryText)
+        )
+    }
+}
+
+#Preview {
+    Group {
+        SearchBarView(searchText: .constant(""))
+            .preferredColorScheme(.dark)
+    }
+}
--- Crypro/Extensions/UIApplication+EndEditing.swift
+++ Crypro/Extensions/UIApplication+EndEditing.swift
@@ -4,5 +4,12 @@
 //
 //  Created by Anton Petrov on 03.04.2024.
 //
+// TODO: Check Relevance
 
-import Foundation
+import SwiftUI
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
